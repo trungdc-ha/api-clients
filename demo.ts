780f364586937ee4a71b4f50497590850e82ad10
trungdc-ha <dangchitrungha@gmail.com>
2023-06-30 08:35:58 +0700

    Updates

---
+++ demo.ts
@@ -0,0 +1,14 @@
+let str:string;
+let arr:[boolean];
+let arr1:Array<number>;
+
+str = "1";
+
+class Student {
+    private id:number;
+    protected name:string;
+}
+
+interface Person {
+    id:number;
+}
